18f3d6362214e30030433cc8fa8042bd16afe3e2
PrasadKodeincloud <Prasad19sara@gmail.com>
2014-05-20 00:01:37 +0530

    Added accounts and added template to the header

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -1,4 +1,8 @@
-
+/**
+ * [pageTitle description]
+ * @param  {[type]} ){return Session.get('pageTitle');}} [description]
+ * @return {[type]}           [description]
+ */
 Template.layout.helpers({
 	pageTitle:function(){return Session.get('pageTitle');}
 });
